c055f9855624c85ca9f530ca0dc7b779fe05e187
PaiDaXing <335015996@qq.com>
2021-04-11 20:09:57 +0800

    11111

---
+++ xinguan-system/src/main/java/com/coderman/system/mapper/RoleMapper.java
@@ -0,0 +1,13 @@
+package com.coderman.system.mapper;
+
+
+import com.coderman.common.model.system.Role;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author wrh
+ * @Date 2021/3/7 15:54
+ * @Version 1.0
+ **/
+public interface RoleMapper extends Mapper<Role> {
+}
